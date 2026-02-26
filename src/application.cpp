8cd59315d252cb271ff36b40a1c18b4fa7c0e4ad
necro8chelse <darkvorontv@gmail.com>
2026-02-26 20:00:17 +0700

    runner for application

---
+++ src/application.cpp
@@ -0,0 +1,47 @@
+#include "../include/application.hpp"
+#include <iostream>
+#include <thread>
+
+namespace app
+{
+    void Application::Run()
+    {
+        std::thread producer(&Application::ProducerTask, this);
+        std::thread consumer(&Application::ConsumerTask, this);
+
+        producer.join();
+        consumer.join();
+    }
+    void Application::ProducerTask()
+    {
+        std::string input;
+        while(true)
+        {
+            std::cout << "Enter value (exit/quit to stop): " << std::endl;
+            std::getline(std::cin, input);
+
+            if (input == EXIT_COMMAND or input == QUIT_COMMAND)
+            {
+                m_queue.Push(std::string(STOP_SIGNAL));
+                break;
+            }
+            m_queue.Push(input);
+        }
+    }
+    void Application::ConsumerTask()
+    {
+        processing::Validator validator;
+
+        while(true)
+        {
+            const std::string value = m_queue.Pop();
+
+            if (value == STOP_SIGNAL)
+            {
+                break;
+            }
+
+            bool is_valid = validator.Validate(value);
+        }
+    }
+}
